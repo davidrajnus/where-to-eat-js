412e0121fcd2999facd791897c4803c48ca49183
DRAJ <davidrajnus@gmail.com>
2017-04-12 22:24:27 +0800

    Completed challenge

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
 function addRestaurantListener(){
-  startBtn = document.getElementById('start-random-button');
+  //create an element to put the result in the end
+  divCenter = document.getElementsByClassName('center');
+  paragraph = document.createElement('p');
+  paragraph.setAttribute("id", "result");
+  paragraph.innerHTML = "";
+  divCenter[0].appendChild(paragraph);
+
+
+  
+  function startRandom(){
+  	//Array of the choices we would like to randomise through
+  	array = ["Nasi Kukus", "KFC", "McD", "BK", "Jaipur", "Fish & Chips"]
+  	//Assuming there was already content in the result area, we put back to
+  	//an empty string
+  	paragraph = document.getElementById('result');
+  	paragraph.innerHTML = "";
+
+  	//We will use final_input to display a result in the circle
+  	final_input = document.getElementById("restaurant");
+
+  	//A loop that iterates the randomise function
+  	interval_id = setInterval(function() {
+  		rand_num = Math.floor(Math.random()*array.length);
+  		console.log(rand_num)
+  		//Display the random restaurant
+  		final_input.innerHTML = array[rand_num]
+
+  	},300)
+
+  }
+
+  function chooseRestaurant(){
+  	//Stops the loop
+  	clearInterval(interval_id);
+  	center = document.getElementsByClassName("center");
+  	restaurant = final_input.textContent;
+  	result = document.getElementById('result')
+  	//Displays the result with an additional string
+  	result.innerHTML = "Great! Let's go to " + restaurant;
+
+  }
   
   // Replace 'functionName' with the name of the function that you have written
-  startBtn.addEventListener("click", functionName)
+  startBtn = document.getElementById('start-random-button');
+  startBtn.addEventListener("click", startRandom)
 
   getBtn = document.getElementById('get-restaurant-button');
-  
-  // Replace 'functionName' with the name of the function that you have written
-  getBtn.addEventListener("click", functionName);
+  getBtn.addEventListener("click", chooseRestaurant);
 }
 
 // wait for DOM content to load before binding an event
